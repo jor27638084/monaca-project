bd33bc204b48334afe03da96c6a855d0b2a4e279
MatheusVedovelli <matheusvedovelli@hotmail.com>
2020-03-05 08:20:50 +0900

    calculadorinha

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
-  
+  $(".btn").click(function(){
+    let val = $(this).attr("value");
+
+    if(val == "C")
+      $("#display").val("");
+    else if(val == "=")
+      $("#display").val(eval($("#display").val()));
+    else
+      $("#display").val($("#display").val() + val);
+  });
 })
